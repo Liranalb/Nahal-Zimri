a3fe0c4b0a83cd5c5afa6b76dad1fd66075b0334
TzuriR <tzurir111@gmail.com>
2020-06-02 22:27:17 +0300

    added files – AdminButton.js AdminTextIn.js

---
+++ components/AdminButton.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {TextInput, View,Text, StyleSheet, TouchableOpacity,Button } from 'react-native';
+
+class AdminButton extends Component {
+    render() {
+        return (
+            <View style={{ width: "40%", flex: 2, paddingLeft: 3, paddingTop: 10 , flexDirection:'row'}}>
+                <View style={styles.editButtons}>
+                        <Button
+                            title="ערוך "
+                            color="green"
+                            onPress={() => this.editText()}
+                        />
+                    </View>
+
+                    <View style={styles.editButtons}>
+                        <Button
+                            title="מחק "
+                            color="green"
+                            onPress={() => this.editText()}
+                        />
+                    </View>
+
+            
+            </View>
+
+        );
+    }
+}
+export default AdminButton;
+
+const styles = StyleSheet.create({
+    
+    editButtons:{
+        paddingRight:1,
+       
+    }
+
+});
