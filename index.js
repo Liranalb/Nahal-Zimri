bf300d1fb6ee8b44dd6bf6888a286974195d3f5e
rolv1 <rotemsnir9@gmail.con>
2020-06-04 22:05:20 +0300

    files

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import LoginForm from "./components/LoginForm"
+import Reports from "./components/Reports"
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LoginForm);
+AppRegistry.registerComponent(appName, () => Reports);
 
 
 
