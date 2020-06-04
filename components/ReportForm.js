d0674ab78282b2b77bd5f8c3d383a85063414a2b
rolv1 <rotemsnir9@gmail.con>
2020-06-04 22:57:25 +0300

    removing accordion - not working

--- components/ReportForm.js
+++ components/ReportForm.js
@@ -1,36 +1,38 @@
 import React, { Component } from "react"
 import { TextInput, Alert, ScrollView, Text, TouchableOpacity } from "react-native"
 import { View } from "native-base"
-import { Header, ListItem, CheckBox,Button } from "react-native-elements"
+import { Header, ListItem, CheckBox, Button } from "react-native-elements"
 import ReportBox from "./explore/ReportBox"
 import LogoHeaderComponent from "./explore/LogoHeaderComponent"
 import Icon from 'react-native-vector-icons/Entypo';
 import HeaderComp from "./HeaderComp"
+import Accordion from "react-native-accordion"
 //import ImagePicker from 'react-native-image-picker';
-
+// let Accordion = require('react-native-accordion');
 
 
 class ReportForm extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            text: "",
-            username: "",
-            catagory: "",
-            loading: false
-        }
-    }
-             //   backgroundColor="#FAE5D3"
+
+    //   backgroundColor="#FAE5D3"
     render() {
         return (
-            <View style = {{backgroundColor: '#FAE5D3'}}>
+            <View style={{ backgroundColor: '#FAE5D3' }}>
+                <View>
+                    
+                </View>
+                <View>
+                    
+                </View>
+                <View>
+                    
+                </View>
+                <Accordion
+                    header={header}
+                    content={content}
+                    easing="easeOutCubic"
+                />
                 
-
-           
-
-
-
             </View>
         )
     }
@@ -42,6 +44,6 @@ class ReportForm extends Component {
 export default ReportForm;
 
 const styles = {
-   
+
 
 }
