c0ce6567c7ca3f44800a3b7208c3c6a417d32540
Liranalb <liran.alb@gmail.com>
2020-06-05 16:29:48 +0300

    Upadte index and add react-navigation

--- index.js
+++ index.js
@@ -4,8 +4,12 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import LoginForm from "./components/LoginForm"
+
 import {name as appName} from './app.json';
+import LoginForm from './components/LoginForm';
+import RegForm from './components/RegForm';
+import MainLogin from './components/MainLogin';
+
 
 AppRegistry.registerComponent(appName, () => LoginForm);
 
