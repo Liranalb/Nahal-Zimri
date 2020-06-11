d54101ea185fb8469a37e0e7c343b92544cbc2ca
Liranalb <liran.alb@gmail.com>
2020-06-11 22:01:37 +0300

    add a method for checking login status

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import App from './App';
 import LoginForm from "./components/LoginForm"
 import HomePageUser from "./components/HomePageUser"
+import firebase from "./config/Firebase"
+
 // import AdminInfoUser from "./components/AdminInfoUser"
 // import AdminUnitInfoUser from"./components/AdminUnitInfoUser"
 
@@ -48,4 +50,35 @@ import HeaderComp from "./components/HeaderComp";
 class App extends Component {
 }
 */
-AppRegistry.registerComponent(appName, () =>MainLogin);
+
+class loginHelper extends Component {
+
+    state = {loggedIn: false}
+
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user) {
+                this.setState({loggedIn: true})
+            }
+            else {
+                this.setState({loggedIn: false})
+            }
+        })
+    }
+
+    renderContent() {
+
+        if(!this.state.loggedIn) {
+            return (<MainLogin/>)
+        }
+        else return <HomePageUser/>
+    }
+
+    render () {
+        return (
+            this.renderContent()
+        )
+    }
+}
+
+AppRegistry.registerComponent(appName, () => loginHelper);
