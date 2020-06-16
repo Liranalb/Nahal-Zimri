787da5ef2aa04f8f905d3ca2e5db50967c9e5c10
TzuriR <tzurir111@gmail.com>
2020-06-16 09:52:17 +0300

    changed file

--- components/RoutesUser.js
+++ components/RoutesUser.js
@@ -1,166 +1,79 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react"
 import { Header, CheckBox, ListItem } from "react-native-elements"
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
-import { Image, View, TextInput, Text, StyleSheet, ScrollView, TouchableWithoutFeedback, Button, Alert, unstable_enableLogBox } from "react-native"
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack'
+import { View } from "native-base"
+import { Image, TextInput, Text, StyleSheet, ScrollView, TouchableWithoutFeedback, Button, Alert, unstable_enableLogBox } from "react-native"
 import { Footer, Container, Right } from "native-base"
-import HeaderComp from "./HeaderComp";
-import NewOpenRoute from "./NewOpenRoute";
+import HeaderComp from "./HeaderComp"
+import NewOpenRoute from "./NewOpenRoute"
 import ReportBox from "./explore/ReportBox"
+import { db } from '../config/Firebase'
+import UnitRoutes from "./UnitRoutes"
+
+var currItem;
+var currImg;
 
 function RoutesUserScreen({ navigation }) {
-    /*constructor() {
-        super();
-        this.state = {
-            text: "",
-            username: "",
-            catagory: "",
-            loading: false
+    let routesArray = [];
+    const [loaded, setLoaded] = useState(false);
+
+    //load data
+    let data = null;
+    db.ref('Routes').on('value', function (snapshot) {
+        const exist = (snapshot.val() !== null);
+        if (exist) {
+            data = snapshot.val();
+            console.log("data loaded: " + loaded);
+            if (loaded === false)
+                setLoaded(true);
+
         }
-    }*/
+    });
+
+
+
+
+    let convertDataToArray = (data, routesArray) => {
+        if (data === null)
+            return null;
+        for (var route in data) {
+            if (data.hasOwnProperty(route)) {
+                routesArray.push(data[route]);
+            }
+        }
+
+    }
+
+    convertDataToArray(data, routesArray);
+
     return (
         <View style={{ width: "100%", height: "100%", backgroundColor: '#FAE5D3' }}>
             <View>
                 <HeaderComp />
             </View>
             <ScrollView>
-                <TouchableWithoutFeedback onPress={() => navigation.navigate('newOpRo')}>
-                    <View style={styles.routeStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/map.png')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>שם המסלול</Text>
-                            <Text style={styles.textDetailStyle}>גל-קראוס שביל ימין</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>רמת קושי</Text>
-                            <Text style={styles.textDetailStyle}>בינוני</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>ק"מ</Text>
-                            <Text style={styles.textDetailStyle}>2.5</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>משך זמן הליכה</Text>
-                            <Text style={styles.textDetailStyle}>40 דקות</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>סוג המסלול</Text>
-                            <Text style={styles.textDetailStyle}>מעגלי</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> פרטים</Text>
-                            <Text style={styles.textDetailStyle}>כל הציבור מוזמן</Text>
-                        </View>
-                    </View>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback onPress={() => navigation.navigate('newOpRo')}>
-                    <View style={styles.routeStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/map.png')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>שם המסלול</Text>
-                            <Text style={styles.textDetailStyle}>שביל לאורך הפריחה</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>רמת קושי</Text>
-                            <Text style={styles.textDetailStyle}>קשה</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>ק"מ</Text>
-                            <Text style={styles.textDetailStyle}>4</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>משך זמן הליכה</Text>
-                            <Text style={styles.textDetailStyle}>70 דקות</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>סוג המסלול</Text>
-                            <Text style={styles.textDetailStyle}>הלוך חזור</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>פרטים</Text>
-                            <Text style={styles.textDetailStyle}>מסלול למיטיבי לכת</Text>
-                        </View>
-                    </View>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback onPress={() => navigation.navigate('newOpRo')}>
-                    <View style={styles.routeStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/map.png')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>שם המסלול</Text>
-                            <Text style={styles.textDetailStyle}>ביקור בבית הצבאים</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>רמת קושי</Text>
-                            <Text style={styles.textDetailStyle}>קל</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>ק"מ</Text>
-                            <Text style={styles.textDetailStyle}>2</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>משך זמן הליכה</Text>
-                            <Text style={styles.textDetailStyle}>35 דקות</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>סוג המסלול</Text>
-                            <Text style={styles.textDetailStyle}>קו</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>פרטים</Text>
-                            <Text style={styles.textDetailStyle}>טיול מהנה</Text>
-                        </View>
-
-                    </View>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback onPress={() => navigation.navigate('newOpRo')}>
-                    <View style={styles.routeStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/map.png')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>שם המסלול</Text>
-                            <Text style={styles.textDetailStyle}>גל-קראוס שביל שמאל</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>רמת קושי</Text>
-                            <Text style={styles.textDetailStyle}>בינוני</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>ק"מ</Text>
-                            <Text style={styles.textDetailStyle}>3.5</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>משך זמן הליכה</Text>
-                            <Text style={styles.textDetailStyle}>60 דקות</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}>סוג המסלול</Text>
-                            <Text style={styles.textDetailStyle}>הלוך-חזור</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> פרטים</Text>
-                            <Text style={styles.textDetailStyle}>כל הציבור מוזמן</Text>
-                        </View>
-                    </View>
-                </TouchableWithoutFeedback>
+                {
+                    console.log("second"),
+                    routesArray.map((item) => {
+                        return (
+                            <View>
+                                <TouchableWithoutFeedback onPress={() => {navigation.navigate('newOpRo'); currItem = item;  currImg={ uri: item.imageLink }}}>
+                                    <View>
+                                        <UnitRoutes imageUri={{ uri: item.imageLink }}
+                                            name={item.name}
+                                            level={item.level}
+                                            km={item.km}
+                                            duration={item.duration}
+                                            type={item.type}
+                                            details={item.details}
+                                        />
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            </View>
+                        )
+                    })
+                }
             </ScrollView>
         </View>
     )
@@ -168,21 +81,21 @@ function RoutesUserScreen({ navigation }) {
 
 function NewOpenRouteScreen() {
     return (
-        <NewOpenRoute />
+        <NewOpenRoute item={currItem} img={currImg}/>
     );
 }
 
 const logStack = createStackNavigator();
 
-function RoutesUser() { //for navigation. not in use yet
+function RoutesUser() { 
     return (
-            <logStack.Navigator initialRouteName="routesU">
-                <logStack.Screen options={{ headerShown: false }} name="routesU" component={RoutesUserScreen} />
+        <logStack.Navigator initialRouteName="routesU">
+            <logStack.Screen options={{ headerShown: false }} name="routesU" component={RoutesUserScreen} />
 
-                <logStack.Screen name="newOpRo" options={{ headerShown: false }}
-                    component={NewOpenRouteScreen} />
+            <logStack.Screen name="newOpRo" options={{ headerShown: false }}
+                component={NewOpenRouteScreen}/>
 
-            </logStack.Navigator>
+        </logStack.Navigator>
     );
 }
 
