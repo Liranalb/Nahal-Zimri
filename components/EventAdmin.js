1f231496a291de192c7b5cd62a9fc8ddd765022e
idonissimian <63245643+idonissimian@users.noreply.github.com>
2020-06-18 10:31:59 +0300

    Update EventAdmin.js

--- components/EventAdmin.js
+++ components/EventAdmin.js
@@ -1,225 +1,129 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react";
 import { Header, ListItem } from "react-native-elements"
 /*import { createStackNavigator } from 'react-navigation-stack';*/
 import { Image, View, TextInput, Text, StyleSheet, ScrollView, TouchableOpacity, Button, Alert, unstable_enableLogBox } from "react-native"
 import { Footer, Container, Right } from "native-base"
 import HeaderComp from "./HeaderComp";
 import AdminButton from "./AdminButton";
+import { db } from '../config/Firebase'
+import EventBoxAdmin from "./EventBoxAdmin";
 
-class EventAdmin extends Component {
-    render() {
-        return (
-            <View style={{ width: "100%", height: "100%", backgroundColor: '#FAE5D3' }}>
-                <View>
-                    <HeaderComp />
-                </View>
-                <ScrollView>
-                    <View style={styles.eventStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/ev1.jpeg')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                            <View style={styles.textStyle}>
-                                <AdminButton />
-                            </View>
-                        </View>
-
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שם האירוע</Text>
-                            <Text style={styles.textDetailStyle}>תצפית על ציפורים</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> תאריך</Text>
-                            <Text style={styles.textDetailStyle}>2.2.20</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> יום</Text>
-                            <Text style={styles.textDetailStyle}>שישי</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שעה</Text>
-                            <Text style={styles.textDetailStyle}>11:00</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> מיקום</Text>
-                            <Text style={styles.textDetailStyle}>רחוב קראוס</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> פרטים</Text>
-                            <Text style={styles.textDetailStyle}>כל הציבור מוזמן</Text>
-                        </View>
-
-                    </View>
-
-                    <View style={styles.eventStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/ev2.jpeg')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                            <View style={styles.textStyle}>
-                                <AdminButton />
-                            </View>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שם האירוע</Text>
-                            <Text style={styles.textDetailStyle}>הרצאה בנושא הסתיו</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> תאריך</Text>
-                            <Text style={styles.textDetailStyle}>1.1.20</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> יום</Text>
-                            <Text style={styles.textDetailStyle}>חמישי</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שעה</Text>
-                            <Text style={styles.textDetailStyle}>14:00</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> מיקום</Text>
-                            <Text style={styles.textDetailStyle}>ספריית פסגת זאב</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> פרטים</Text>
-                            <Text style={styles.textDetailStyle}>עלות : 20 שח לאדם</Text>
-                        </View>
-                    </View>
-                    <View style={styles.eventStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/ev3.jpeg')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                            <View style={styles.textStyle}>
-                                <AdminButton />
-                            </View>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שם האירוע</Text>
-                            <Text style={styles.textDetailStyle}>הרצאה בנושא החי בנחל</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> תאריך</Text>
-                            <Text style={styles.textDetailStyle}>11.12.19</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> יום</Text>
-                            <Text style={styles.textDetailStyle}>שלישי</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שעה</Text>
-                            <Text style={styles.textDetailStyle}>20:00</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> מיקום</Text>
-                            <Text style={styles.textDetailStyle}>מינהל קהילתי פסגת זאב</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> פרטים</Text>
-                            <Text style={styles.textDetailStyle}>הרצאה מעניינת, מומלץ להגיע!</Text>
-                        </View>
-                    </View>
-                    <View style={styles.eventStyle}>
-                        <View style={styles.imageStyle}>
-                            <Image
-                                source={require('../assets/img/ev4.jpeg')}
-                                style={{ width: "100%", height: "100%" }}
-                            />
-                            <View style={styles.textStyle}>
-                                <AdminButton />
-                            </View>
-
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שם האירוע</Text>
-                            <Text style={styles.textDetailStyle}>סיור בעקבות הצבאים</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> תאריך</Text>
-                            <Text style={styles.textDetailStyle}>10.10.19</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> יום</Text>
-                            <Text style={styles.textDetailStyle}>שני</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> שעה</Text>
-                            <Text style={styles.textDetailStyle}>9:30</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> מיקום</Text>
-                            <Text style={styles.textDetailStyle}>רחוב קראוס</Text>
-                        </View>
-                        <View style={styles.textStyle}>
-                            <Text style={styles.textTitleStyle}> פרטים</Text>
-                            <Text style={styles.textDetailStyle}>כל הציבור מוזמן</Text>
-                        </View>
-                    </View>
-
-                    <View>
-                        <TouchableOpacity onPress={() => navigation.navigate('newOpAr')}>
-
-                            <View style={{ alignItems: "center" }}>
-                                <Text style={{ fontWeight: "bold" }} > טען יותר...</Text>
-                            </View>
-                        </TouchableOpacity>
-
-                        <Text>שם האירוע:</Text>
-                        <  TextInput
-                            style={styles.textInput}
-                            onChangeText={text => onChange(text)}
-                        //value={detail}
-                        />
-                        <Text>תאריך:</Text>
-                        <  TextInput
-                            style={styles.textInput}
-                            onChangeText={text1 => onChange1(text1)}
-                        //value={detail1}
-                        /><Text>יום:</Text>
-                        <  TextInput
-
-                            style={styles.textInput}
-                            onChangeText={text2 => onChange2(text2)}
-                        //value={detail2}
-                        />
-                        <Text>שעה:</Text>
-                        <  TextInput
-
-                            style={styles.textInput}
-                            onChangeText={text2 => onChange2(text2)}
-                        //value={detail2}
-                        />
-                        <Text>מיקום:</Text>
-                        <  TextInput
-
-                            style={styles.textInput}
-                            onChangeText={text2 => onChange2(text2)}
-                        //value={detail2}
-                        />
-                        <Text>פרטים:</Text>
-                        <  TextInput
-
-                            style={{ height: 80, borderColor: 'gray', borderWidth: 1, backgroundColor: 'white' }}
-                            onChangeText={text2 => onChange2(text2)}
-                        //value={detail2}
-                        />
+function EventAdmin() {
+    let eventsArray = [];
+    const [loaded, setLoaded] = useState(false);
 
 
-                    </View>
-                    <View style={styles.buttonStyle}>
-                        <TouchableOpacity>
-                            <Text style={{ color: 'white' }}>הוסף</Text>
-                        </TouchableOpacity >
-                    </View>
-                </ScrollView>
-            </View>
+    //load data
+    let data = null;
+    db.ref('Events').on('value', function (snapshot) {
+        const exist = (snapshot.val() !== null);
+        if (exist) {
+            data = snapshot.val();
+            console.log("data loaded: " + loaded);
+            if (loaded === false)
+                setLoaded(true);
+
+        }
+    });
 
 
-        )
+
+
+    let convertDataToArray = (data, eventsArray) => {
+        if (data === null)
+            return null;
+        for (var event in data) {
+            if (data.hasOwnProperty(event)) {
+                eventsArray.push(data[event]);
+            }
+        }
+
     }
+
+    convertDataToArray(data, eventsArray);
+
+    return (
+        <View style={{ width: "100%", height: "100%", backgroundColor: '#FAE5D3' }}>
+            <View>
+                <HeaderComp />
+            </View>
+            <ScrollView>
+                {
+                    console.log("second"),
+                    eventsArray.map((item) => {
+                        return (
+                            <EventBoxAdmin imageUri={{ uri: item.imageLink }}
+                                name={item.name}
+                                date={item.date}
+                                weekday={item.weekday}
+                                hour={item.hour}
+                                location={item.location}
+                                details={item.details}
+                            />
+                        )
+                    })
+                }
+                <View>
+                    <TouchableOpacity onPress={() => navigation.navigate('newOpAr')}>
+
+                        <View style={{ alignItems: "center" }}>
+                            <Text style={{ fontWeight: "bold" }} > טען יותר...</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <Text>שם האירוע:</Text>
+                    <  TextInput
+                        style={styles.textInput}
+                        onChangeText={text => onChange(text)}
+                    //value={detail}
+                    />
+                    <Text>תאריך:</Text>
+                    <  TextInput
+                        style={styles.textInput}
+                        onChangeText={text1 => onChange1(text1)}
+                    //value={detail1}
+                    /><Text>יום:</Text>
+                    <  TextInput
+
+                        style={styles.textInput}
+                        onChangeText={text2 => onChange2(text2)}
+                    //value={detail2}
+                    />
+                    <Text>שעה:</Text>
+                    <  TextInput
+
+                        style={styles.textInput}
+                        onChangeText={text2 => onChange2(text2)}
+                    //value={detail2}
+                    />
+                    <Text>מיקום:</Text>
+                    <  TextInput
+
+                        style={styles.textInput}
+                        onChangeText={text2 => onChange2(text2)}
+                    //value={detail2}
+                    />
+                    <Text>פרטים:</Text>
+                    <  TextInput
+
+                        style={{ height: 80, borderColor: 'gray', borderWidth: 1, backgroundColor: 'white' }}
+                        onChangeText={text2 => onChange2(text2)}
+                    //value={detail2}
+                    />
+
+
+                </View>
+                <View style={styles.buttonStyle}>
+                    <TouchableOpacity>
+                        <Text style={{ color: 'white' }}>הוסף</Text>
+                    </TouchableOpacity >
+                </View>
+            </ScrollView>
+        </View>
+
+
+    )
+
 }
 
 export default EventAdmin;
@@ -242,8 +146,8 @@ const styles = {
         borderColor: "green",
         borderWidth: 2,
         fontSize: 10,
-        width: "40%",
-        height: "3%",
+        width: "30%",
+        height: "6%",
         alignSelf: "center",
         marginTop: 5,
         marginBottom: "10%",
