bacbfffb6ee1e1c05bd3b3613f927bb844478372
Liranalb <liran.alb@gmail.com>
2021-03-11 15:30:11 +0200

    create user maps screens and connect them to the maps db

---
+++ components/Information/MapsBoxUser.js
@@ -0,0 +1,145 @@
+
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    Linking,
+    Alert
+
+} from "react-native";
+import { Divider } from "react-native-elements";
+
+class MapBoxUser extends React.Component {
+    render() {
+        return (
+            <View style={styles.mapStyle}>
+                <View style={{ flex: 1}}>
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', }}>
+                        <Image
+                            source={this.props.imageUri}
+                            style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
+                        />
+                    </View>
+                </View >
+
+                <View style={{ flex: 2 }}>
+                    <View style={styles.textStyle}>
+                        <Text style={styles.textTitleStyle}>שם המפה: </Text>
+                        <Text numberOfLines={1} style={styles.textDetailStyle}>{this.props.name} </Text>
+                    </View>
+                    <Divider/>
+                    <View style={styles.textStyle}>
+                        <Text style={styles.textTitleStyle}>מיקום: </Text>
+                        <Text numberOfLines={1} style={styles.textDetailStyle}>{this.props.location}</Text>
+                    </View>
+                    <Divider/>
+                    <View style={styles.textStyle}>
+                        <Text style={styles.textTitleStyle}>פרטים: </Text>
+                        <Text numberOfLines={1} style={styles.textDetailStyle}>{this.props.details}</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => { 
+                        if(this.props.link === undefined)
+                            Alert.alert("", "אין פרטים נוספים לאירוע זה");
+                        else
+                            Linking.openURL(this.props.link); 
+                        }}>
+
+                    <View style={styles.buttonStyle}>
+                            <Text
+                                style={styles.buttonText}>פתיחת המפה</Text>
+                            
+                    </View>      
+
+                    </TouchableOpacity>          
+                </View>
+
+            </View>
+
+        );
+    }
+}
+export default MapBoxUser;
+
+const styles = StyleSheet.create({
+
+    mapStyle: {
+        backgroundColor: "#F4D5A7",
+        borderColor: "#FFAF50",
+        overflow: 'hidden',
+        borderRadius: 15,
+        borderWidth: 1.1,
+        fontSize: 20,
+        marginTop: "2%",
+        width:'100%',
+        //height:155,
+        flexDirection:'row-reverse'
+    },
+
+    textStyle: {
+        flexDirection: 'row',
+        //flex:1
+
+    },
+    ButtonStyle: {
+        width: "69%",
+        // marginLeft:100,
+
+
+    },
+    detailStyle: {
+        height: 50,
+        width: "69%",
+        marginLeft: 111,
+        height: "75%",
+
+    },
+    dateStyle: {
+        height: 30,
+        width: "50%",
+        marginLeft: 180,
+        marginTop: 10
+
+    },
+    textTitleStyle: {
+        
+        fontWeight: "bold",
+        fontSize: 18,
+        marginLeft: 2
+ 
+    },
+    textDetailStyle: {
+        //fontWeight: "normal",
+        fontSize: 16,
+        flex:1,
+        textAlign:'left'
+
+        //alignSelf: "center"
+    },
+
+    buttonText: {
+        alignSelf: 'center',
+        fontSize: 20, 
+        color: 'white', 
+        marginRight: "3%", 
+        marginLeft: "3%"
+    },
+
+    buttonStyle: {
+        justifyContent: 'center',
+        alignItems: "center",
+        alignSelf: "center",
+        backgroundColor: "#FF8C37",
+        borderColor: "#FF8C37",
+        borderRadius: 10,
+        borderWidth: 2,
+        alignSelf: "center",
+        marginTop: "2%",
+        marginBottom: "3%",
+        
+    }
+
+});
+
---
+++ components/Information/MapsUser.js
@@ -0,0 +1,137 @@
+import React, { Component, useState, useEffect, useLayoutEffect } from "react";
+import { RefreshControl, ScrollView, Text, TouchableOpacity } from "react-native";
+import { View } from "native-base";
+import HeaderComp from "../explore/HeaderComp";
+//import firebase from "../config/Firebase";
+import { db } from '../../config/Firebase';
+import MapBoxUser from './MapsBoxUser';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerContent } from "../DrawerContent";
+
+function wait(timeout) {
+    return new Promise(resolve => {
+        setTimeout(resolve, timeout);
+    });
+}
+
+function MapUserScreen({ navigation }) {
+    const [refreshing, setRefreshing] = useState(false);
+    let mapsArray = [];
+    const [loaded, setLoaded] = useState(false);
+
+
+    //load data
+    let data = null;
+    db.ref('Maps').on('value', function (snapshot) {
+        const exist = (snapshot.val() !== null);
+        if (exist) {
+            data = snapshot.val();
+            console.log("data loaded: " + loaded);
+            if (loaded === false)
+                setLoaded(true);
+
+        }
+    });
+
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+
+        wait(1000).then(() => setRefreshing(false));
+    }, [refreshing]);
+
+
+    let convertDataToArray = (data, mapsArray) => {
+        if (data === null)
+            return null;
+        for (var map in data) {
+            if (data.hasOwnProperty(map)) {
+                mapsArray.push(data[map]);
+            }
+        }
+
+    }
+
+    convertDataToArray(data, mapsArray);
+
+    return (
+        <View style={{ width: "100%", height: "100%", backgroundColor: '#FAE5D3'}}>
+            <HeaderComp
+                openUserProfile={() => navigation.navigate('Current')}
+                openUserMenu={() => navigation.dangerouslyGetParent().openDrawer()}
+            />
+            <ScrollView
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+            >
+                {
+                    console.log("second"),
+                    mapsArray.map((item) => {
+                        return (
+                            <View key={item.id} style={{ width: "96%", alignSelf: 'center', marginTop: "1%" }}>
+                                <MapBoxUser imageUri={{ uri: item.imageLink }}
+                                    name={item.name}
+                                    location={item.location}
+                                    details={item.details}
+                                    link={item.link}
+                                />
+                            </View>
+                        )
+                    })
+                }
+            </ScrollView>
+        </View>
+    )
+}
+
+const DrawerMap = createDrawerNavigator();
+
+function MapUserPage() {
+    return (
+        <DrawerMap.Navigator initialRouteName="reports" drawerPosition="right"
+            drawerStyle={{ width: '45%' }} drawerContent={props => <DrawerContent {...props} />}>
+            <DrawerMap.Screen name="mapPage" component={MapUserScreen} />
+
+        </DrawerMap.Navigator>
+
+    );
+}
+
+
+export default MapUserPage;
+
+const styles = {
+    eventStyle: {
+        backgroundColor: "#F6D365",
+        borderColor: "#FFAF50",
+        overflow: 'hidden',
+        borderRadius: 15,
+        borderWidth: 2,
+        fontSize: 20,
+        marginTop: 10
+    },
+    imageStyle: {
+        marginTop: 10,
+        marginLeft: 10,
+        borderColor: "#FFAF50",
+        position: 'absolute',
+        borderWidth: 4,
+        height: "85%",
+        width: "30%"
+    },
+    textStyle: {
+        flexDirection: 'row-reverse'
+    },
+    textTitleStyle: {
+        alignSelf: "center",
+        fontWeight: "bold",
+        fontSize: 20,
+        marginLeft: 10
+    },
+    textDetailStyle: {
+        fontWeight: "normal",
+        fontSize: 16,
+        alignSelf: "center"
+    }
+}
+
+
